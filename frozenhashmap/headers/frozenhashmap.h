3d0e6c6ae9411cbc3c30cc0526db93d34434ef0c
benAmi <minebenking@gmail.com>
2024-01-07 10:59:15 +0200

    added docs to frozenhashmap.h file

--- frozenhashmap/headers/frozenhashmap.h
+++ frozenhashmap/headers/frozenhashmap.h
@@ -4,19 +4,36 @@
 #include "linkedlist.h"
 #include "hashutils.h"
 
+/// @brief The FrozenHashMap struct
 typedef struct {
     struct HashBucket *buckets;
     unsigned int capacity;
 } FrozenHashMap;
 
+/// @brief Creates a new hashmap
+/// @param capacity: The capacity of the hashmap
+/// @return The new hashmap
 FrozenHashMap *frozen_hash_map(unsigned int);
 
+/// @brief Looks up a key in the map
+/// @param fhm: The FrozenHashMap
+/// @param key: The key to lookup
+/// @return A pointer to the `HashEntry`
 HashEntry *fh_lookup(FrozenHashMap *, char *);
 
+/// @brief Inserts an item into the map
+/// @param fhm: The FrozenHashMap
+/// @param key: The key to insert
+/// @param item: The item to insert
 void fh_insert(FrozenHashMap *, char *, void *);
 
+/// @brief Deletes an item from the map
+/// @param fhm: The FrozenHashMap
+/// @param key: The key to delete
 void fh_delete(FrozenHashMap *, char *);
 
+/// @brief Frees a FrozenHashMap
+/// @param fhm: The FrozenHashMap to free
 void free_fhm(FrozenHashMap *);
 
 #endif
